734c4f3c29d9724e9934c24aaabbc29e6aa394fe
Santiago Noguera <santiiago.noguera@gmail.com>
2026-03-27 13:14:25 -0300

    feat: create placeholder for Comment, Post and Tag requests

---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; // Adjust authorization as needed
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [];
+    }
+}
