209cb1f904a6ef2c5f665721fc51f18e73b7f40d
Santiago Noguera <santiiago.noguera@gmail.com>
2026-03-27 13:13:19 -0300

    feat: add createApplication function on TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,27 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Laravel\Fortify\Features;
 
 abstract class TestCase extends BaseTestCase
 {
+    use RefreshDatabase;
+
+    /**
+     * Creates the application.
+     */
+    public function createApplication(): Application
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
     protected function skipUnlessFortifyFeature(string $feature, ?string $message = null): void
     {
         if (! Features::enabled($feature)) {
