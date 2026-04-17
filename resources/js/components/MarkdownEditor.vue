fd46deb1d6947ba67d182d1f966073cf6ea3004a
Santiago Noguera <santiiago.noguera@gmail.com>
2026-04-17 15:47:39 -0300

    feat: add boilerplate to MarkdownEditor.vue

---
+++ resources/js/components/MarkdownEditor.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+const markdown = {
+    content: ''
+};
+
+const compiledMarkdown = {
+    content: '',
+};
+
+</script>
+
+<template>
+    <div id="editor">
+        <textarea v-model="markdown.content" name="markdown-input" id="markdown-input"></textarea>
+        <div v-html="compiledMarkdown.content"></div>
+    </div>
+</template>
