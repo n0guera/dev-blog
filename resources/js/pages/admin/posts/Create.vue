9bb29d61cba2b99a33d141ab853f97f15ac08409
Santiago Noguera <santiiago.noguera@gmail.com>
2026-04-17 17:20:34 -0300

    feat: include MarkdownEditor in Create post page

--- resources/js/pages/admin/posts/Create.vue
+++ resources/js/pages/admin/posts/Create.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import MarkdownEditor from '@/components/MarkdownEditor.vue';
 defineProps<{ statuses: any }>();
 </script>
 
 <template>
     <div>Create Post</div>
+    <MarkdownEditor />
 </template>
