fcff44038f144de212c213670fdbac4c308e684e
Santiago Noguera <santiiago.noguera@gmail.com>
2026-05-07 22:46:54 -0300

    feat: add PostForm component

---
+++ resources/js/components/PostForm.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import MarkdownEditor from './MarkdownEditor.vue';
+
+defineProps<{
+    form: any,
+    statuses: Array<any>,
+}>()
+</script>
+
+<template>
+    <!-- Title -->
+    <div>
+        <label>
+            Title
+        </label>
+
+        <input v-model="form.title" type="text" placeholder="Title">
+        <p v-if="form.errors.title">{{ form.errors.title }}</p>
+    </div>
+
+    <!-- Status -->
+    <div>
+        <label>
+            Status
+        </label>
+
+        <select v-model="form.status_id">
+            <option v-for="status in statuses" :key="status.id" :value="status.id">
+                {{ status.name }}</option>
+        </select>
+
+        <p v-if="form.errors.status_id">
+            {{ form.errors.status_id }}
+        </p>
+    </div>
+
+    <!-- Content -->
+    <div>
+        <label>
+            Content
+        </label>
+
+        <MarkdownEditor v-model="form.content" />
+        <p v-if="form.errors.content"> {{ form.errors.content }}</p>
+    </div>
+</template>
--- resources/js/pages/admin/posts/Create.vue
+++ resources/js/pages/admin/posts/Create.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
-import { Head } from '@inertiajs/vue3';
-import MarkdownEditor from '@/components/MarkdownEditor.vue';
+import { Head, useForm } from '@inertiajs/vue3';
+import PostController from '@/actions/App/Http/Controllers/Admin/PostController';
+import PostForm from '@/components/PostForm.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
+
 defineProps<{ statuses: any }>();
+
+const form = useForm({
+    title: '',
+    content: '',
+    status: ''
+});
+
+const submit = () => {
+    form.post(PostController.store().url);
+}
 </script>
 
 <template>
@@ -10,6 +22,9 @@ defineProps<{ statuses: any }>();
     <Head title="Create Post" />
     <AppLayout>
         <h1 class="mb-2">Create Post</h1>
-        <MarkdownEditor />
+        <form @submit.prevent="submit">
+            <PostForm :form="form" :statuses="statuses" />
+            <button type="submit" :disabled="form.processing">Create</button>
+        </form>
     </AppLayout>
 </template>
